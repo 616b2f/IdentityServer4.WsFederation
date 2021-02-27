9da626ed16011e9a40c5a0478f5c2a284dc84f30
Alexej Kowalew <616b2f@gmail.com>
2021-02-27 19:15:05 +0100

    fix wrong wsignin message parameter for IdP was used

--- samples/server/IdentityServer4.WsFederation/Startup.cs
+++ samples/server/IdentityServer4.WsFederation/Startup.cs
@@ -22,7 +22,8 @@ namespace IdentityServer4.WsFederation
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients())
                 .AddTestUsers(TestUsers.Users)
-                .AddWsFederation();
+                .AddWsFederation()
+                .AddInMemoryRelyingParties(Config.GetRelyingParties());
 
             services.AddAuthentication()
                 .AddGoogle("Google", "Google", options =>
